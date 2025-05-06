5667ec771130c2415161139708cee4522b2fa5ec
bykovskii <bykovskii@trueconf.com>
2025-05-06 20:41:27 +0300

    feat: add 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,40 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
+  const values1 = ['q', 'w', 'e', 'r', 't', 'y'];
+  const values2 = [1, 2, 3, 4, 5, 6];
+
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const expectedLinkedList = {
+      value: 'q',
+      next: {
+        value: 'w',
+        next: {
+          value: 'e',
+          next: {
+            value: 'r',
+            next: {
+              value: 't',
+              next: {
+                value: 'y',
+                next: {
+                  value: null,
+                  next: null,
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+
+    expect(generateLinkedList(values1)).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(values2)).toMatchSnapshot();
   });
 });
